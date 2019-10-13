0a65fe2e3394ee2aa78ebcb8e9ba3aa0c68ecee0
noureldien <nreldien@yahoo.com>
2019-10-13 14:18:42 +0200

    commit

--- main.py
+++ main.py
@@ -27,3 +27,8 @@ Main file of the project.
 # train videograph of features of epic_kitchens
 from experiments import epic_kitchens
 epic_kitchens.train_model_videograph()
+
+
+# train videograph of features of epic_kitchens
+# from experiments import charades
+# charades.train_model_videograph()
